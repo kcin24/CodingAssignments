fe1375578016f922ccdf87f163f5382aa35f3df2
Nikhil Pillai <pillai.n@northeastern.edu>
2021-09-26 19:48:18 -0400

    Committing file

---
+++ src/Model/Form.java
@@ -0,0 +1,167 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author nickk
+ */
+public class Form {
+    String name;
+    String location;
+    String dob;
+    String tele;
+    String fax;
+    String email;
+    String ssn;
+    String medRecNum;
+    String healthNum;
+    String bankAccount;
+    String licenseNum;
+    String vehicleNum;
+    String deviceNum;
+    String linkedin;
+    String ip;
+    String photo;
+    String uniqueIdNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+
+    public void setTele(String tele) {
+        this.tele = tele;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedRecNum() {
+        return medRecNum;
+    }
+
+    public void setMedRecNum(String medRecNum) {
+        this.medRecNum = medRecNum;
+    }
+
+    public String getHealthNum() {
+        return healthNum;
+    }
+
+    public void setHealthNum(String healthNum) {
+        this.healthNum = healthNum;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getLicenseNum() {
+        return licenseNum;
+    }
+
+    public void setLicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    public String getVehicleNum() {
+        return vehicleNum;
+    }
+
+    public void setVehicleNum(String vehicleNum) {
+        this.vehicleNum = vehicleNum;
+    }
+
+    public String getDeviceNum() {
+        return deviceNum;
+    }
+
+    public void setDeviceNum(String deviceNum) {
+        this.deviceNum = deviceNum;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getUniqueIdNum() {
+        return uniqueIdNum;
+    }
+
+    public void setUniqueIdNum(String uniqueIdNum) {
+        this.uniqueIdNum = uniqueIdNum;
+    }
+    
+}
